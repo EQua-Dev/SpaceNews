1abb23c89e4b634a54f4d8bb8ef675d577a122a3
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2023-07-17 10:03:20 +0100

    Initial Commit

---
+++ SpaceNews/SpaceNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsApp.swift
+//  SpaceNews
+//
+//  Created by Richard Uzor on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
